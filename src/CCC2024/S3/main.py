1f1b7ca99de7ce774e2fa3816e5a6ea35e803b07
Saksham Goel <saksham.goel29@gmail.com>
2024-02-29 20:13:00 -0500

    updated, must add pivot check

--- src/CCC2024/S3/main.py
+++ src/CCC2024/S3/main.py
@@ -1,6 +1,7 @@
 N = int(input())
 a = list(map(int, input().split(" ")))
 o = list(map(int, input().split(" ")))
+sO = set(o)
 imp = True
 ex = False
 
@@ -19,11 +20,26 @@ N = len(a)
 8
 1 1 1 1 2 1 2 1
 2 1 1 2 1 1 1 1
+
+2 2 2 2 2 1 2 1 L
+2 1 1 1 1 1 2 1 L
+2 1 1 2 2 2 2 1 L
+2 1 1 2 1 1 1 1 L
 """
+
+
+def pivotCheck(arr: list, a: int, b:int):
+
 
 
 def recall(arr: list, K: int, ins: str):
     global com
+
+    sArr = set(arr)
+    for i in sO:
+        if i not in sArr:
+            return
+
     # print(arr)
     add = True
     index = 0
@@ -33,35 +49,31 @@ def recall(arr: list, K: int, ins: str):
             break
         index += 1
     if add:
-        com.append([arr.copy(), set()])
+        com.append([arr.copy(), set(), set()])
         index = len(com) - 1
+        # print(index)
     for i in range(N):
-        for j in range(i, N):
+        for j in range(N):
             if i == j:
                 continue
-            if j > i:
-                c = len(com[index][1])
-                rS = f"\nR {i} {j}"
-                com[index][1].add(rS)
 
-                c2 = len(com[index][1])
-                if c2 - c != 0:
-                    c = c2
-                    swipes(arr.copy(), K + 1, ins + rS)
+            rS = f"\nR {i} {j}"
+            c = len(com[index][1])
+            com[index][1].add(rS)
+            if len(com[index][1]) - c != 0:
+                swipes(arr.copy(), K + 1, ins + rS)
 
-                lS = f"\nL {j} {i}"
-                com[index][1].add(rS)
-
-                c2 = len(com[index][1])
-                if c2 - c != 0:
-                    swipes(arr.copy(), K + 1, ins + lS)
-            else:
-                continue
+            lS = f"\nL {i} {j}"
+            c = len(com[index][2])
+            com[index][2].add(lS)
+            if len(com[index][2]) - c != 0:
+                swipes(arr.copy(), K + 1, ins + lS)
 
 
 def swipes(arr: list, K: int, ins: str):
     global a, imp
     arrC = arr.copy()
+
     if K > N:
         return
     elif K == 0:
@@ -73,14 +85,18 @@ def swipes(arr: list, K: int, ins: str):
     insList = ins.split("\n")
     curIns = insList[-1].split(" ")
     c = curIns[0]
-    l = int(curIns[1])
-    r = int(curIns[2])
+    a = int(curIns[1])
+    b = int(curIns[2])
     if c == "R":
-        for i in range(l + 1, r + 1):
+        # print(arr, a, b)
+        for i in range(a + 1, b + 1):
             arr[i] = arr[i - 1]
+        # print(arr, a, b, "\n")
     elif c == "L":
-        for i in range(r, l):
-            arr[i] = arr[i + 1]
+        # print(arr, a, b)
+        for i in range(b, a):
+            arr[i] = arr[a]
+        # print(arr, a, b, "\n")
 
     if arr == arrC:
         return
@@ -97,5 +113,5 @@ if not ex:
     swipes(a.copy(), 0, "")
     if imp:
         print("NO")
-        if (a, o) != ([1, 2], [2, 1]):
-            print(a, o)
+        # if (a, o) != ([1, 2], [2, 1]):
+        #     print(ao, o)
